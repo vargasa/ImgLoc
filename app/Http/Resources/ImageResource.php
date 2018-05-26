d45ff2e85693ff2a56a13414b0a59df1931c9b22
Andres Vargas <vargasa@jlab.org>
2018-05-26 17:04:24 -0400

    Resources added

---
+++ app/Http/Resources/ImageResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ImageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+	    'id' => $this->id,
+		'url' => $this->filename,
+		'latitude' => $this->latitude,
+		'longitude' => $this->longitude,
+	];
+    }
+}
